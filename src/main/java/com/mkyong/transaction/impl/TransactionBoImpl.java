96b49502c73ddc30256105955746cc37a6fbaa50
markiewb <benno.markiewicz@googlemail.com>
2013-08-13 14:05:38 +0200

    working sample J2EE 1.4, Spring 3.x, Jersey * based on http://www.mkyong.com/webservices/jax-rs/jersey-spring-integration-example/

---
+++ src/main/java/com/mkyong/transaction/impl/TransactionBoImpl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mkyong.transaction.impl;
+
+import com.mkyong.transaction.TransactionBo;
+
+public class TransactionBoImpl implements TransactionBo {
+
+    @Override
+    public String save() {
+
+        return "Jersey + Spring example";
+
+    }
+
+}
