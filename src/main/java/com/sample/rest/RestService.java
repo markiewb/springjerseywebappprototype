5721781dacf3520b8f189c18490990398d35954e
markiewb <benno.markiewicz@googlemail.com>
2013-08-13 21:07:21 +0200

    json tryout

--- src/main/java/com/sample/rest/RestService.java
+++ src/main/java/com/sample/rest/RestService.java
@@ -2,7 +2,6 @@ package com.sample.rest;
 
 import com.sample.service.MyService;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
